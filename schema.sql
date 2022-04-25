4dcad75383df7790297cb617a0f3682a9c0e166d
Victor Peralta <victor.peralta.gomez@gmail.com>
2022-04-25 13:19:16 -0500

    Add primary key for animal table, change name data type to varchar(100)
    modified:   README.md
    new file:   animals-table.png
    modified:   schema.sql

--- schema.sql
+++ schema.sql
@@ -7,10 +7,13 @@
 DROP TABLE IF EXISTS animals;
 CREATE TABLE animals (
 id INT GENERATED ALWAYS AS IDENTITY,
-name text NOT NULL,
+name varchar(100),
 date_of_birth DATE,
 escape_attempts INTEGER default 0,
 neutered BOOLEAN default false,
 weight_kg DECIMAL not NULL
 );
+ALTER TABLE animals 
+ADD CONSTRAINT PK_animals
+PRIMARY KEY (id)
 
