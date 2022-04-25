c37a945c397c0936327f3272e14e090cd3e53e39
Victor Peralta <victor.peralta.gomez@gmail.com>
2022-04-25 13:25:13 -0500

    Revert "Create animals table"

--- schema.sql
+++ schema.sql
@@ -1,19 +1,5 @@
 /* Database schema to keep the structure of entire database. */
 
--- CREATE DATABASE
--- CREATE DATABASE vet_clinic;
--- USE vet_clinic;
-
-DROP TABLE IF EXISTS animals;
 CREATE TABLE animals (
-id INT GENERATED ALWAYS AS IDENTITY,
-name varchar(100),
-date_of_birth DATE,
-escape_attempts INTEGER default 0,
-neutered BOOLEAN default false,
-weight_kg DECIMAL not NULL
+    name varchar(100)
 );
-ALTER TABLE animals 
-ADD CONSTRAINT PK_animals
-PRIMARY KEY (id)
-
