65da4895c5e08ff73453197e87ad9cb7155a7bde
Victor Peralta <victor.peralta.gomez@gmail.com>
2022-04-25 19:52:12 -0500

    Update queries to query and update animals table
    modified:   data.sql
    modified:   queries.sql
    modified:   schema.sql

--- queries.sql
+++ queries.sql
@@ -1,27 +1,79 @@
-/*Queries that provide answers to the questions from all projects.*/
+-- Inside a transaction update the animals table by setting the species column to unspecified. 
+-- Verify that change was made. Then roll back the change and verify that species columns 
+-- went back to the state before transaction.
 
--- USE vet_clinic;
--- Find all animals whose name ends in "mon".
-SELECT * FROM animals WHERE name LIKE '%mon';
+-- setting the species column to unspecified
+BEGIN TRANSACTION;
+UPDATE animals SET species='unspecified';
+-- Verify that change was made
+SELECT * FROM animals ORDER BY id; 
+-- column specied contains unspecified
+-- roll back the change
+ROLLBACK;
+-- column specied contains null
 
--- List the name of all animals born between 2016 and 2019.
-SELECT name FROM animals WHERE EXTRACT(YEAR FROM date_of_birth) BETWEEN 2016 AND 2019;
+-- Inside a transaction
+-- Update the animals table by setting the species column to digimon for all animals 
+-- that have a name ending in mon.
 
--- List the name of all animals that are neutered and have less than 3 escape attempts.
-SELECT name FROM animals WHERE neutered=true AND escape_attempts<3;
+BEGIN TRANSACTION;
+UPDATE animals SET species='digimon' WHERE name LIKE '%mon' ;
 
--- List date of birth of all animals named either "Agumon" or "Pikachu".
-SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
+-- Update the animals table by setting the species column to pokemon for all animals 
+-- that don't have species already set.
+UPDATE animals SET species='pokemon' WHERE species IS NULL;
+-- Commit the transaction.
+COMMIT TRANSACTION;
+-- change was made and persists after commit.
+SELECT * FROM animals ORDER BY id;
 
---List name and escape attempts of animals that weigh more than 10.5kg
-SELECT name, escape_attempts FROM animals WHERE weight_kg>10.5;
+BEGIN TRANSACTION;
+DELETE FROM animals;
 
--- Find all animals that are neutered.
-SELECT * FROM animals WHERE neutered=true;
+-- this query returns none
+SELECT * FROM animals ORDER BY id;
+ROLLBACK;
+-- this query returns all records
+SELECT * FROM animals ORDER BY id;
 
--- Find all animals not named Gabumon.
-SELECT * FROM animals WHERE name!='Gabumon';
+-- inside a transaction
+BEGIN TRANSACTION;
+-- Delete all animals born after Jan 1st, 2022.
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SELECT * FROM animals ORDER BY id;
+-- Create a savepoint for the transaction.
+SAVEPOINT BEFORE_WEIGHT_CHANGE;
+-- Update all animals' weight to be their weight multiplied by -1.
+UPDATE animals SET weight_kg = (weight_kg * -1);
+SELECT * FROM animals ORDER BY id;
+-- Rollback to the savepoint
+ROLLBACK TO BEFORE_WEIGHT_CHANGE
+SELECT * FROM animals ORDER BY id;
+-- Update all animals' weights that are negative to be their weight multiplied by -1.
+UPDATE animals SET weight_kg = (weight_kg * -1) WHERE weight_kg <0;
+-- all weights positives
+SELECT * FROM animals ORDER BY id;
+COMMIT TRANSACTION;
 
--- Find all animals with a weight between 10.4kg and 17.3kg 
--- (including the animals with the weights that equals precisely 10.4kg or 17.3kg)
-SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg<=17.3;
+-- How many animals are there? (10)
+SELECT COUNT(name) FROM animals;
+
+-- How many animals have never tried to escape? (2)
+SELECT COUNT(name) FROM animals WHERE escape_attempts=0;
+
+-- What is the average weight of animals? (15.567)
+SELECT AVG(weight_kg) AS "average weight" FROM animals;
+
+-- Who escapes the most, neutered or not neutered animals? (Neutered 20)
+SELECT CASE 
+WHEN neutered=true THEN 'Neutered' ELSE 'Not Neutered' 
+END,
+SUM(escape_attempts) as "escapes" FROM animals GROUP BY neutered ORDER BY escapes DESC LIMIT 1;
+
+-- What is the minimum and maximum weight of each type of animal?
+SELECT species, MIN(weight_kg) AS "Minimum weight", MAX(weight_kg)  AS "Maximum weight" FROM animals GROUP BY species;
+
+-- What is the average number of escape attempts per animal type of those born 
+-- between 1990 and 2000? (3)
+SELECT species, AVG(escape_attempts) FROM animals WHERE EXTRACT(YEAR FROM date_of_birth) 
+BETWEEN 1990 AND 2000 GROUP BY species;
